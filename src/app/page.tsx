eff6310e86f7ff3384d218ba26654b233829e950
ChamudiNavodya <chamudinavodya14@gmail.com>
2024-07-01 01:50:38 +0530

    Change size and color of home page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <main>homepage</main>;
-}
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br/> <span className="text-accent"> Chamudi Navodya</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+
+            </p>
+          </div>
+
+          {/* photo */}
+          <div>photo</div>
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
