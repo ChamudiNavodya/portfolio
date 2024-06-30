ba5c5a3ac1af05ce78f07584c97399043da7eb59
ChamudiNavodya <chamudinavodya14@gmail.com>
2024-07-01 01:55:53 +0530

    Add Description to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,15 @@ const Home = () => {
               Hello I'm <br/> <span className="text-accent"> Chamudi Navodya</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
+            I am a highly motivated and dedicated Information Technology
+            undergraduate at SLIIT, passionate about leveraging technology
+            to solve real-world issues. Proficient in Java, Python, and C++,
+            adept in database management and web development.
+            Proactive, adaptive, and eager to explore the intersection of
+            technology and business. Committed to continuous learning,
+            with a strong foundation in IT concepts and a proactive
+            approach to problem-solving. Seeking opportunities to apply
+            skills and contribute positively to the IT sphere.
 
             </p>
           </div>
