3406f2131dbcb2ac5551bd9b270ee0a003afb098
ChamudiNavodya <chamudinavodya14@gmail.com>
2024-06-30 19:12:26 +0530

    Add Animation

--- src/components/Stairs.tsx
+++ src/components/Stairs.tsx
@@ -1,7 +1,21 @@
-const Stairs = () => {
-    return (
-        <div>Stairs</div>
-    )
-};
+import {motion} from "framer-motion";
 
+//variants
+const stairAnimation = {
+    initial: {
+        top: "0%",
+    },
+    animate: {
+        top: "100%",
+    },
+    exit: {
+        top: ["100%" , "0%"],
+    },
+
+};
+const Stairs = () => {
+    return <>
+
+    </>;
+};
 export default Stairs;
