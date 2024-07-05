ee701f9f83631901333940c9f3f2dfb8747cefe7
ChamudiNavodya <chamudinavodya14@gmail.com>
2024-07-05 23:19:57 +0530

    change circle location

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -19,7 +19,7 @@ const Photo = () => {
                 opacity: 1,
                 transition: { delay: 2.4, duration: 0.4, ease: 'easeInOut' }
                 }}
-                className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
+                className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten relative">
             
                 <Image 
                     src="/picme.jpg" 
@@ -52,7 +52,7 @@ const Photo = () => {
                             rotate: [120, 360],
                         }}
                         transition={{
-                            duration: 30,
+                            duration: 20,
                             repeat: Infinity,
                             repeatType: "reverse",
                         }}
