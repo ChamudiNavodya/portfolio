9789149e0bf0abaf3f8a3404675b50cf5a3f5889
ChamudiNavodya <chamudinavodya14@gmail.com>
2024-07-06 22:26:21 +0530

    change photo component code

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,38 +1,38 @@
-"use client";
+"use client"
 
-import {motion} from "framer-motion";
-import Image from "next/image";
+import { motion } from "framer-motion"
+import Image from "next/image"
 
 const Photo = () => {
-    return (<div className="w-full h-full relative">
-        <motion.div 
-            initial={{ opacity: 0 }}
-            animate={{
-            opacity: 1,
-            transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' }
-            }}
-            >
-                {/* image */}
-            <motion.div 
+    return (
+        <div className="w-full h-full relative">
+            <motion.div
                 initial={{ opacity: 0 }}
                 animate={{
-                opacity: 1,
-                transition: { delay: 2.4, duration: 0.4, ease: 'easeInOut' }
+                    opacity: 1,
+                    transition: { delay: 2, duration: 0.4, ease: 'easeIn' }
                 }}
-                className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten relative">
-            
-                <Image 
-                    src="/picme.jpg" 
-                    priority
-                    quality={100}
-                    fill
-                    alt=""
-                    className="object-contain"
-                />
-            </motion.div>
+            >
+                {/* image */}
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{
+                        opacity: 1,
+                        transition: { delay: 2.4, duration: 0.4, ease: 'easeInOut' }
+                    }}
+                    className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
+                    <Image
+                        src="/assets/Photo.png"
+                        priority
+                        quality={100}
+                        fill
+                        alt=""
+                        className="object-contain"
+                    />
+                </motion.div>
 
-              {/* circle */}
-              <motion.svg
+                {/* circle */}
+                <motion.svg
                     className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
                     fill="transparent"
                     viewBox="0 0 506 506"
@@ -58,13 +58,9 @@ const Photo = () => {
                         }}
                     />
                 </motion.svg>
+            </motion.div>
+        </div>
+    )
+}
 
-
-        </motion.div>
-    </div>
-    );
-        
-    
-};
-
-export default Photo;
+export default Photo
