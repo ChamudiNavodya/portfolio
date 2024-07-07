455508344e2a7915a3d5e0bc74f82afabf57ad18
ChamudiNavodya <chamudinavodya14@gmail.com>
2024-07-07 10:38:15 +0530

    Add services to service page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,3 +1,45 @@
+"use client";
+
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link"
+
+const services = [
+    {
+      num: "01",
+      title: "Web development",
+      description:
+        "Transform your vision into reality with cutting-edge web development. Let's create engaging and responsive websites together!",
+      href: "",
+    },
+
+    {
+        num: "02",
+        title: "UI/UX design",
+        description:
+          "Elevate your digital presence with intuitive and visually stunning UI/UX design. Let's craft exceptional user experiences together!",
+        href: "",
+      },
+      
+    {
+      num: "03",
+      title: "Mobile development",
+      description:
+        "Unlock the potential of your ideas with expert mobile development. Let's build seamless and innovative mobile experiences together!",
+      href: "",
+    },
+  
+    {
+      num: "04",
+      title: "Figma design",
+      description:
+        "Bring your concepts to life with expert Figma design. Let's create visually compelling and user-friendly interfaces together!",
+      href: "",
+    },
+  ];
+
+
+
+
 const Services = () => {
     return (
         <div>services page</div>
